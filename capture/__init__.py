ff8f8dcdbf9c9710334182097e1e410555cc0248
Gabor Szabo <gabor@szabgab.com>
2023-03-21 21:19:21 +0200

    decode

--- capture/__init__.py
+++ capture/__init__.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def separated(command):
+def separated_bytes(command):
     '''
     >>> exit_code, out, err = separated(['ls', '-l'])
     '''
@@ -12,6 +12,9 @@ def separated(command):
     exit_code = proc.returncode
     return exit_code, out, err
 
+def separated(command):
+    exit_code, out, err = separated_bytes(command)
+    return exit_code, out.decode('utf8'), err.decode('utf-8')
 
 def merged(command):
     '''
